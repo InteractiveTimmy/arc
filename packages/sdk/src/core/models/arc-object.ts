b466c3ebc6ae1204e2991ac4faedd4bb8f7b2d7e
InteractiveTimmy <interactivetimmy@gmail.com>
2020-02-21 14:48:55 -0500

    docs(@arc/sdk): add root object documentation

--- packages/sdk/src/core/models/arc-object.ts
+++ packages/sdk/src/core/models/arc-object.ts
@@ -1,8 +1,20 @@
 import { uuid } from '../utils/index';
 
+/**
+ * Abstract ArcObject Class.
+ *
+ * Provides a root object for all other models within this project to extend
+ * upon.
+ */
 export default abstract class ArcObject {
+  /**
+   * The unique identifier for this {@link ArcObject}.
+   */
   public readonly uuid: string;
 
+  /**
+   * Generate a new {@link ArcObject}.
+   */
   public constructor() {
     this.uuid = uuid();
   }
